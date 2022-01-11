0310531495ff4bf4594457830a169681417677ba
Kozlov ALexandr <kozlovalexander21@gmail.com>
2022-01-11 12:15:03 +0300

    Check link systemapplications

--- CreateWinWeb.py
+++ CreateWinWeb.py
@@ -3,5 +3,18 @@
 # Импортирую библиотеку sys для работы с CLI
 
 import sys
+import mmap
 
-print("Hello World")
+# Поиск system.applicationHost
+def check():
+    with open('C:\\Users\\alexandr.kozlov\\Desktop\\applicationHost.config') as f:
+        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+
+# s.find ведет себя как  Bytearray поэтому необходимо добавить b
+        if s.find(b"<system.applicationHost>") != -1:
+            print('true')
+        else:
+            print('false')
+
+
+check()
