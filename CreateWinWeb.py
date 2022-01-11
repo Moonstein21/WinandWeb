4d685c7fce54458417edb463bbf9d8a2c44b32c7
Kozlov ALexandr <kozlovalexander21@gmail.com>
2022-01-11 13:27:37 +0300

    Add function for get informations

--- CreateWinWeb.py
+++ CreateWinWeb.py
@@ -2,10 +2,13 @@
 
 # Импортирую библиотеку sys для работы с CLI
 
-import sys
 import mmap
 
+# Словарь для записи информации
+data = {}
+
 # Поиск system.applicationHost
+
 def check():
     with open('C:\\Users\\alexandr.kozlov\\Desktop\\applicationHost.config') as f:
         s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
@@ -16,5 +19,32 @@ def check():
         else:
             print('false')
 
+def information():
+    answer = 'yes'
+    while answer == 'yes':
+        if answer == 'yes':
+            print("Вы хотите создать Web или Win сервис?(web/win) ")
+            serv = input()
+            if serv == "web":
+                print("Введите наименование Web сервиса: ")
+                Nameweb = input()
+                print("\nВведите имя учетной записи: ")
+                Nameuser = input()
+                print("\nВведите пароль от учетной записи: ")
+                password = input()
+                print("\n Хотите ли добавить еще application Pool?(yes/no) ")
+                answer = input()
+                data[Nameweb] = [Nameuser, password, "web"]
+            else:
+                print("Введите наименование Win сервиса: ")
+                Namewin = input()
+                print("\nВведите имя учетной записи: ")
+                Nameuser = input()
+                print("\nВведите пароль от учетной записи: ")
+                password = input()
+                print("\n Хотите ли добавить еще application Pool?(yes/no) ")
+                answer = input()
+                data[Namewin] = [Nameuser, password, "win"]
+    print(data)
 
-check()
+information()
