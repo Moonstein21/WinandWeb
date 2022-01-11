bbf14ec16fcc547b726411a3b0e33b9749cb6425
Kozlov ALexandr <kozlovalexander21@gmail.com>
2022-01-11 16:56:01 +0300

    add dictionary and add in file version 1

--- CreateWinWeb.py
+++ CreateWinWeb.py
@@ -32,9 +32,14 @@ def information():
                 Nameuser = input()
                 print("\nВведите пароль от учетной записи: ")
                 password = input()
+                print("\nВведите адрес расположения папки: ")
+                phisicalPath = input()
+                print("\nВведите номер порта: ")
+                port = input()
                 print("\n Хотите ли добавить еще application Pool?(yes/no) ")
                 answer = input()
-                data[Nameweb] = [Nameuser, password, "web"]
+                data[Nameweb] = {'login': Nameuser, 'password': password, 'type': 'web', 'path': phisicalPath,
+                                 'port': port, 'name': Nameweb}
             else:
                 print("Введите наименование Win сервиса: ")
                 Namewin = input()
@@ -47,4 +52,16 @@ def information():
                 data[Namewin] = [Nameuser, password, "win"]
     print(data)
 
+def writefileiis():
+    with open(r'C:\\Users\\alexandr.kozlov\\Desktop\\1.txt', "w") as file:
+        for i in data:
+            if data[i]["type"] == "web":
+                file.write(str("<add name = \"" + data[i]["name"] + "\" autoStart=\"true\" managedRuntimeVersion=\"\">" + '\n'
+                           + "<processModel userName=\"" + data[i]["login"] + "\" password=\"" + data[i]["password"]
+                           + "\" />\n" + "</add>\n"))
+
+
+
 information()
+writefileiis()
+
